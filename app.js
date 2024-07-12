be07aacc18cac687f5a0f841269656766137822a
Debtosh Roychowdhury <debatoshofficial85@gmail.com>
2024-07-12 23:00:23 +0530

    netlify

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ import {errorMiddleware} from './middlewares/error.js'
 const app = express();
 dotenv.config({path:"./config/config.env"})
 app.use(cors({
-    origin : ["https://hireminds.netlify.app/","http://localhost:5173"],
+    origin : ["https://hireminds.netlify.app","http://localhost:5173"],
     methods: ["GET","POST", "PUT","DELETE"],
     credentials: true,
 }))
