dd8ba9af73541bda4def06695a0684d9bfa4a4ac
Debtosh Roychowdhury <debatoshofficial85@gmail.com>
2024-07-12 22:47:56 +0530

    netlify

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ import {errorMiddleware} from './middlewares/error.js'
 const app = express();
 dotenv.config({path:"./config/config.env"})
 app.use(cors({
-    origin : ["https://hireminds.vercel.app", "https://hireminds-debatosh-roychowdhurys-projects.vercel.app","http://localhost:5173"],
+    origin : ["https://hireminds.vercel.app", "https://hireminds.netlify.app/","http://localhost:5173"],
     methods: ["GET","POST", "PUT","DELETE"],
     credentials: true,
 }))
