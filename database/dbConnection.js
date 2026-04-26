5669bb697b316ca9e7613747adda0e3227cc265d
Debatosh Roychowdhury <debatoshoffical85@gmail.com>
2026-04-27 03:09:42 +0530

    fixed mongodb connection and auth bug

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -4,6 +4,10 @@ export const dbConnection = () => {
   mongoose
     .connect(process.env.MONGO_URI, {
       dbName: "MERN_JOB_SEEKING_WEBAPP",
+      serverSelectionTimeoutMS: 30000,
+      socketTimeoutMS: 45000,
+      retryWrites: true,
+      w: "majority",
     })
     .then(() => {
       console.log("Connected to database.");
